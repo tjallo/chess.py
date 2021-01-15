a2826ae327a58c04287af4a971d8a8bdd59e6922
Tjalle <31376864+tjallo@users.noreply.github.com>
2021-01-15 17:59:06 +0100

    init

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import os
 import chess
 import numpy as np
 import pandas as pd
-from tensorflow import keras
-from tensorflow.keras import layers
+# from tensorflow import keras
+# from tensorflow.keras import layers
 from chessboard import display
 
 board = chess.Board()
 
-while True:
-    display.start(board.fen())
+legalmoves = [move for move in board.legal_moves]
+
+print(legalmoves)
