6a369ef51a80677ce418d1965712fae743445cf8
Tjalle <31376864+tjallo@users.noreply.github.com>
2021-01-15 17:50:05 +0100

    simple chessboard

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ import numpy as np
 import pandas as pd
 from tensorflow import keras
 from tensorflow.keras import layers
+from chessboard import display
+
+board = chess.Board()
+
+while True:
+    display.start(board.fen())
